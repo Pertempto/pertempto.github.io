65dd76d1647a738b2a7b364bb6f270e389c43336
Pertempto <pertempto@github.com>
2024-10-12 15:45:53 -0400

    first experiments with astro

---
+++ projects/astro/src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Growing Along the Way';
+export const SITE_DESCRIPTION = 'The personal site of Addison Emig.';
